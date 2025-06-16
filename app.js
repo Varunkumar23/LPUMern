1312d08cbe5de0c58c8e4ff9016bcae91a385ecc
Varunkumar23 <sarabuvarunkumar@gmail.com>
2025-06-16 13:40:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+window.console.log("Hello World!");
+
+// console.log(age);
+
+// var age=22;
+// console.log(age);
+
+// var age=23;
+// console.log(age);
+
+
+// let age=10;
+
+// console.log(age);
+// let age=16;
+// age=100;
+// console.log(age);
+
+// let x=12;
+// let y='a';
+// console.log(x-y);
+
+
+// let a="varun";
+// let b=" kumar";
+// console.log(a+b);
+
+console.log(1-1);
+console.lo`g('a'-1);
+console.log(1-'a');
+console.log('1'-'a');
+
+console.log(1*1);
+console.log('a'*1);
+console.log(1*'a');
+console.log('1'*'a');
+
+console.log(1/1);
+console.log('a'/1);
+console.log(1/'a');
+console.log('1'/'a');
+
+
+
+
