3bccd7543259b6502058bea1fa9e4a4af68e2355
Varunkumar23 <sarabuvarunkumar@gmail.com>
2025-06-17 09:56:38 +0530

    day2 updated

--- app.js
+++ app.js
@@ -25,21 +25,126 @@ window.console.log("Hello World!");
 // let b=" kumar";
 // console.log(a+b);
 
-console.log(1-1);
-console.lo`g('a'-1);
-console.log(1-'a');
-console.log('1'-'a');
+// console.log(1-1);
+// console.log('a'-1);
+// console.log(1-'a');
+// console.log('1'-'a');
 
-console.log(1*1);
-console.log('a'*1);
-console.log(1*'a');
-console.log('1'*'a');
+// console.log(1*1);
+// console.log('a'*1);
+// console.log(1*'a');
+// console.log('1'*'a');
 
-console.log(1/1);
-console.log('a'/1);
-console.log(1/'a');
-console.log('1'/'a');
+// console.log(1/1);
+// console.log('a'/1);
+// console.log(1/'a');
+// console.log('1'/'a');
 
+
+// =========================================================================
+
+// basic calculations
+
+// let Juice=50;
+// let gstOnJuice=0.1;
+// let fries=70;
+// let gstOnFood=0.05;
+
+// let JuiceBill=Juice+Juice*gstOnJuice;
+// let friesBill=fries+fries*gstOnFood;
+
+// const finalbill=JuiceBill+friesBill;
+// console.log("finalbill:",finalbill);
+
+
+// =============================================================================
+//1. Function Declaration(without parameters and argumenta)
+
+// console.log("START");
+// function calculateJuiceBill(){
+//     let Juice=50;
+//     let gst=0.1;
+//     let JuiceBill=Juice+Juice*gst;
+//     console.log("JuiceBill: ",JuiceBill);
+// }
+
+// console.log("MID");
+// calculateJuiceBill();
+// calculateJuiceBill();
+// calculateJuiceBill();
+// console.log("END");
+
+
+
+//2. Function Declaratin(with parameters and arguments)
+// console.log("START");
+// function calculateJuiceBill(Juice,gst){
+//     // let Juice=50;
+//     // let gst=0.1;
+//     let JuiceBill=Juice+Juice*gst;
+//     console.log("JuiceBill: ",JuiceBill);
+// }
+
+// console.log("MID");
+// calculateJuiceBill(10,0.1);
+// calculateJuiceBill(20,0.2);
+// calculateJuiceBill(30,0.3);
+// console.log("END");
+
+
+//================================================================================
+
+// function printbill(name,amount){
+//     console.log("-------------------");
+//     console.log("Your bill for",name);
+//     console.log("Rs.",amount);
+//     console.log("-------------------");
+// }
+
+// function calculatebillamount(price,gst){
+//     console.log("---calculating---");
+//     let billamount=price+price*gst;
+//     return billamount;
+    
+// }
+
+// let res1=calculatebillamount(60,0.1);
+// printbill("Mango Juice",res1);
+
+// let res2=calculatebillamount(70,0.1);
+// printbill("Mango Juice",res2);
+
+// let res3=calculatebillamount(80,0.1);
+// printbill("Mango Juice",res3);
+
+//================================================================================
+
+//3.Function assignment (named functions) we will assign functions to the variables directly
+//to reduce redeclarations or for any security purpose in future!
+
+
+// const printbill=function (name,amount){
+//     console.log("-------------------");
+//     console.log("Your bill for",name);
+//     console.log("-------------------");
+//     console.log("Rs.",amount);
+// }
+
+// const calculatebillamount=function (price,gst){
+//     console.log("---calculating---");
+//     let billamount=price+price*gst;
+//     return billamount;
+    
+// }
+
+// let res1=calculatebillamount(60,0.1);
+// printbill("Mango Juice",res1);
+
+// let res2=calculatebillamount(70,0.1);
+// printbill("Grape Juice",res2);
+
+// let res3=calculatebillamount(80,0.1);
+// printbill("Orange Juice",res3);
 
 
 
---
+++ day2/app.js
@@ -0,0 +1,71 @@
+// Instead of writing the function syntax in the traditional way there is another way to write the function 
+//which is called ARROW FUNCTIONS
+
+//Arrow function
+
+const printBill=(txt)=>{
+    console.log("------------------");
+    console.log("Rs: ",txt);
+    console.log("------------------");
+
+
+
+}
+const printBillforFoodandDrinks=(txt)=>{
+    console.log("------------------");
+    console.log("Bill for food and drinks");
+    console.log("Rs: ",txt);
+    console.log("------------------");
+
+
+
+}
+const printBillforFoodDrinksandClothes=(txt)=>{
+    console.log("------------------");
+    console.log("Bill for food,drinks and clothes");
+    console.log("Rs: ",txt);
+    console.log("------------------");
+
+
+
+}
+
+// printBill(100);
+//=================================================================================>
+
+const calculateFoodBillAmount=(price1)=>{
+    return price1*1.05;
+}
+
+const calculateClothesBillAmount=(price2)=>{
+    return price2*1.12;
+}
+
+const calculateDrinksBillAmount=(price3)=>{
+    return price3*1.2;
+}
+
+
+// Higher order function
+// The function which accepts another function as a paramenter or returns a function
+const calculateTotalBill=(f,c,d,cb)=>{
+const food=calculateFoodBillAmount(f);
+const clothes=calculateClothesBillAmount(c);
+const drinks=calculateDrinksBillAmount(d);
+
+const finalamount=food+clothes+drinks;
+cb(finalamount);
+//here we are passing one function as a paramenter so that based on different items we purchase it will generate different bills
+}
+
+calculateTotalBill(100,400,100,printBillforFoodDrinksandClothes);
+
+
+
+
+
+
+
+
+
+
